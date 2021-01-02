bf5f4bf59f9a2d074435aaf289fe8db22a470f0e
DevSwaika99 <66863974+DevSwaika99@users.noreply.github.com>
2021-01-02 14:51:09 +0530

    Updated

---
+++ results/script.js
@@ -0,0 +1,38 @@
+document.querySelector(".search-btn").addEventListener('click',searchText);
+document.querySelector(".cancel").addEventListener('click',clearText);
+document.querySelector('.input').addEventListener('keyup',function(event){
+    if (event.keyCode === 13) {
+   event.preventDefault();
+   document.querySelector(".search-btn").click();
+  }
+});
+
+function searchText(){
+{
+        output=""
+        flag=0;
+        var search=document.querySelector('.input').value.toUpperCase();
+        if(search=='19MIS0411')
+            {
+            output+=`
+            <div class="container info">      
+            <p>
+                Congratulations! You have been Shortlisted for Round 2 in the following Domains. 
+            </p> 
+            </div>
+            `
+            flag=1;
+            }
+        };
+
+        if(flag==0)
+        {
+            output=`<h1 class="nfound">Sorry you were not Shortlisted. Try again next year !<h1>`;
+        }
+        document.querySelector('.output').innerHTML=output;
+    }        
+
+function clearText(){
+    document.querySelector('.output').innerHTML='';
+    document.querySelector('.input').value='';
+}
